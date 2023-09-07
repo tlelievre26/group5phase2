0d13819221d9435beb95f33d732672139bb5e7da
yasmin <yshahed@purdue.edu>
2023-09-07 19:54:13 -0400

    Complete data fetcher for Bus Factor metric

--- src/services/metrics-data-retriever.ts
+++ src/services/metrics-data-retriever.ts
@@ -1,9 +1,12 @@
 import { injectable } from "tsyringe";
 import { graphql } from "@octokit/graphql";
 
+
 @injectable()
 export class MetricsDataRetriever {
+
     private graphqlWithAuth: any;
+
 
     constructor(token: string) {
         this.graphqlWithAuth = graphql.defaults({
@@ -12,6 +15,7 @@ export class MetricsDataRetriever {
             }
         });
     }
+
 
     /**
      * Retrieves metrics data for a list of GitHub URLs.
@@ -52,20 +56,80 @@ export class MetricsDataRetriever {
         }));
     }
 
+
+    /**
+     * Fetches bus factor data for a GitHub repository.
+     *
+     * TODO:
+     *   - Handle errors
+     *   - Handle rate limiting/implement retries
+     *
+     * @param owner The owner of the repository.
+     * @param repo The name of the repository.
+     */
     async fetchBusFactorData(owner: string, repo: string): Promise<any> {
 
+        // Get date one year ago
+        const currentDate = new Date();
+        const oneYearAgo = new Date(currentDate.setFullYear(currentDate.getFullYear() - 1)).toISOString();
 
+        // Query GitHub API for contributors in the last year
+        const query = `
+        {
+          repository(owner: "${owner}", name: "${repo}") {
+            defaultBranchRef {
+              target {
+                ... on Commit {
+                  history(since: "${oneYearAgo}") {
+                    totalCount
+                    edges {
+                      node {
+                        author {
+                          user {
+                            login
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+        `;
+
+        const {repository} = await this.graphqlWithAuth(query);
+
+        // Count the number of commits for each unique contributor using a map
+        const contributorCommitFrequency = new Map();
+        repository.defaultBranchRef.target.history.edges.forEach((edge: { node: { author: { user: { login: string; }; }; }; }) => {
+            // Check author is not null before adding contributor to map
+            if (edge.node.author?.user) {
+                const contributor = edge.node.author.user.login;
+                // Increment commit count for contributor
+                contributorCommitFrequency.set(contributor, (contributorCommitFrequency.get(contributor) || 0) + 1);
+            }
+        });
+
+        return {
+            repo: repo,
+            contributorCommitFrequency: contributorCommitFrequency
+        };
     }
+
 
     async fetchCorrectnessData(owner: string, repo: string): Promise<any> {
 
 
     }
 
+
     async fetchRampUpData(owner: string, repo: string): Promise<any> {
 
 
     }
+
 
     async fetchResponsiveMaintainerData(owner: string, repo: string): Promise<any> {
 
