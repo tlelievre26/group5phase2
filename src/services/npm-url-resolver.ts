b8325047ea66e316e529d67901bd9059762ab2ec
yasmin <yshahed@purdue.edu>
2023-09-08 22:23:41 -0400

    Implement URL handlers

--- src/services/npm-url-resolver.ts
+++ src/services/npm-url-resolver.ts
@@ -1,9 +1,27 @@
 import { injectable } from "tsyringe";
 
+
 @injectable()
 export class NpmUrlResolver {
 
-    resolveToGitHub(url: string): void {
+    private PACKAGE_NAME_REGEX = /^https:\/\/www\.npmjs\.com\/package\/([a-z0-9\-_]+)/;
+    private GITHUB_URL_REGEX = /github\.com\/([a-zA-Z0-9\-_]+)\/([a-zA-Z0-9\-_]+)/;
 
+
+    async resolveNpmToGitHub(npmUrl: string): Promise<string> {
+
+        const npmPackageName = npmUrl.match(this.PACKAGE_NAME_REGEX);
+
+        if (!npmPackageName) {
+            throw new Error(`Invalid npm URL: ${npmUrl}`);
+        }
+        const response = await fetch(`https://registry.npmjs.org/${npmPackageName[1]}`);
+        const data = await response.json();
+
+        if (!data.repository?.url) {
+            throw new Error(`No repository URL found for package: ${npmPackageName}`);
+        }
+
+        return data.repository.url.match(this.GITHUB_URL_REGEX)[0];
     }
 }
--- src/services/url-file-processor.ts
+++ src/services/url-file-processor.ts
@@ -3,25 +3,55 @@ import fs from "fs";
 
 import { NpmUrlResolver } from "./npm-url-resolver";
 
+
 @injectable()
 export class UrlFileProcessor {
+
     constructor(
         @inject("NpmUrlResolver") private npmUrlResolver: NpmUrlResolver
     ) {
     }
 
-    public processUrlFile(urlFilePath: string): string[] {
-        const urls = this.extractUrlsFromFile(urlFilePath);
-        // For each url,
-        //     if GitHub url
-        //         then insert in array
-        //     else if npm url
-        //         then resolve npm url to GitHub url by invoking NpmUrlResolver and insert in array
-        //     else throw error
-        return [];
+
+    async processUrlFile(urlFilePath: string): Promise<string[]> {
+        const urls = await this.extractUrlsFromFile(urlFilePath);
+        const processedUrls: string[] = [];
+
+        for (const url of urls) {
+            if (await this.isGitHubUrl(url)) {
+                processedUrls.push(url);
+            } else if (await this.isNpmUrl(url)) {
+                const gitHubUrl = await this.npmUrlResolver.resolveNpmToGitHub(url);
+                processedUrls.push(gitHubUrl);
+            } else {
+                throw new Error(`Unsupported URL type: ${url}`);
+            }
+        }
+
+        return processedUrls;
     }
 
-    extractUrlsFromFile(urlFilePath: string): string[] {
-        return [];
+
+    async isGitHubUrl(url: string): Promise<boolean> {
+        if (!url) {
+            throw new Error("URL is empty in isGitHubUrl");
+        }
+        const githubRegex = /^https:\/\/github\.com\/[a-zA-Z0-9_-]+\/[a-zA-Z0-9_-]+$/;
+        return githubRegex.test(url);
+    }
+
+
+    async isNpmUrl(url: string): Promise<boolean> {
+        if (!url) {
+            throw new Error("URL is empty in isNpmUrl");
+        }
+        const npmRegex = /^https:\/\/www\.npmjs\.com\/package\/[a-zA-Z0-9_-]+$/;
+        return npmRegex.test(url);
+    }
+
+
+    async extractUrlsFromFile(urlFilePath: string): Promise<string[]> {
+        const fileContents = fs.readFileSync(urlFilePath, "utf-8");
+        return fileContents.split("\n").map(line => line.trim()).filter(Boolean);
     }
 }
