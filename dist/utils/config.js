46dba029b8ebf0fab140fe61eb73aa7f26afd890
Taylor James Le Lievre <tlelievr@eceprog1.ecn.purdue.edu>
2023-11-10 20:03:27 -0500

    Finalized buildspec file to complete CD pipeline

--- dist/utils/config.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-//Checks on the environment variables from the .env file
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.GITHUB_TOKEN = void 0;
-if (!process.env.GITHUB_TOKEN) {
-    throw new Error("GITHUB_TOKEN environment variable is required");
-}
-exports.GITHUB_TOKEN = process.env.GITHUB_TOKEN;
-//# sourceMappingURL=config.js.map
