96e6df4dc45e0f2b525104642007167cfd7b5acd
Jack Douglas Gardner <gardne97@eceprog5.ecn.purdue.edu>
2023-11-15 20:04:03 -0500

    Debloating

--- src/services/scoring/services/metrics-calculator.ts
+++ src/services/scoring/services/metrics-calculator.ts
@@ -216,21 +216,6 @@ export class MetricsCalculator {
             RampUpScore += 0.5;
         }
 
-        //Assigns Half of the RampUpScore to how far apart the Readme Update and the Last Commit Are
-        //The Score goes from 0-0.5 for a range of 1 year apart to the same
-        const lastUpdatedDate = new Date(rampUpData.lastUpdated);
-        const lastCommitDate = rampUpData.lastCommit ? new Date(rampUpData.lastCommit) : null;
-        if (lastCommitDate) {
-            // Calculate the absolute time difference in milliseconds
-            const timeDifference = Math.abs(lastUpdatedDate.getTime() - lastCommitDate.getTime());
-
-            // Define a maximum time difference (adjust as needed)
-            const maxTimeDifference = 1000 * 60 * 60 * 24 * 30; // 30 days in milliseconds
-
-            // Calculate the score based on the time difference
-            RampUpScore += Math.max(0, 0.5 - timeDifference / maxTimeDifference);
-        }
-
         return RampUpScore;
     }
 
--- src/services/upload/debloat.ts
+++ src/services/upload/debloat.ts
@@ -1,1 +1,26 @@
-//Debloating function goes here, likely a functionality several endpoints will need
+import JSZip from 'jszip';
+
+export function debloatZip(zip: JSZip): JSZip {
+    var filesToDelete = [
+        'docs/',        // Documentation files
+        'test/',        // Test files
+        'tests/',       // Test files (alternative folder name)
+        'examples/',    // Example files
+        'examples.js',  // Example JavaScript file
+        'CHANGELOG',    // Change log file
+        'CHANGELOG.md', // Change log file (Markdown format)
+        'CONTRIBUTING', // Contributing guidelines
+        'CONTRIBUTING.md', // Contributing guidelines (Markdown format)
+        'yarn.lock',    // Yarn lock file
+        '.gitignore',   // Git ignore file
+        '.travis.yml',  // Travis CI configuration
+        '.editorconfig', // Editor configuration
+        'dist/'        // Distribution files
+    ];
+
+    filesToDelete.forEach(file => {
+        zip.remove(file);
+    });
+
+    return zip;
+}
