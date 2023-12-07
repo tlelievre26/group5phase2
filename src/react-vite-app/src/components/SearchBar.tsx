d0df6f3e1dc747bcf0720866a2187dc1d307577a
Rebecca <rupp3@purdue.edu>
2023-12-07 13:58:49 -0600

    added postcss and starting ADA

--- src/react-vite-app/src/components/SearchBar.tsx
+++ src/react-vite-app/src/components/SearchBar.tsx
@@ -1,31 +1,42 @@
-
-import React, { ChangeEvent } from 'react';
-import "../App.css";
+import React, { ChangeEvent, useState } from 'react';
 import { FaSearch } from 'react-icons/fa';
+
 interface SearchBarProps {
   onSearch: (searchTerm: string) => void;
 }
 
-
 const SearchBar: React.FC<SearchBarProps> = ({ onSearch }) => {
+  const [isLabelVisible, setLabelVisible] = useState(false);
+
   const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     const searchTerm = event.target.value;
     onSearch(searchTerm);
   };
 
   return (
-    <div className="flex items-center space-x-4">
+    <div className="relative flex items-center space-x-4">
       <input
         type="text"
         placeholder="Search for packages"
-        className="border border-gray-300 px-4 py-3 rounded-md focus:outline-none focus:border-blue-500 w-full" // Updated class to w-full
+        className="border border-gray-300 px-4 py-3 rounded-md focus:outline-none focus:border-blue-500 w-full"
         onChange={handleInputChange}
       />
       <button
-        className="bg-blue-500 text-white px-3 py-3 rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue flex items-center"
-        onClick={() => onSearch('')} // You can customize the behavior of the button click
+        type="submit"
+        aria-label="Search"
+        aria-haspopup="true"
+        role="tooltip"
+        className="bg-blue-500 text-white px-3 py-3 rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue flex items-center relative group"
+        onClick={() => onSearch('')}
+        onMouseEnter={() => setLabelVisible(true)}
+        onMouseLeave={() => setLabelVisible(false)}
       >
         <FaSearch className="flex item-center" />
+        {isLabelVisible && (
+          <span className="absolute bg-white text-gray-600 p-2 opacity-100 transition-opacity duration-300 bottom-full left-1/2 transform -translate-x-1/2">
+            Search
+          </span>
+        )}
       </button>
     </div>
   );
