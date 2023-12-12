a89479cb87d90a4606ac20a1f3cc08c10a70ec26
Rebecca <rupp3@purdue.edu>
2023-12-12 02:07:32 -0600

    more ada changes, login not working

--- frontend/src/components/LoginForm.tsx
+++ frontend/src/components/LoginForm.tsx
@@ -1,5 +1,3 @@
-// src/LoginForm.js
-
 import React, { FormEvent, useState } from 'react';
 import { useAuth } from './AuthContext';
 import { Auth } from '../API/Auth'; // Make sure to adjust the path
@@ -8,26 +6,32 @@ const LoginForm: React.FC = () => {
     const { authResult, setAuth } = useAuth();
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [error, setError] = useState<string | null>(null);
 
     const handleLogin = async (event: FormEvent) => {
         event.preventDefault();
-
+    
         try {
             // Use encodeURIComponent to escape special characters in the password
             const pass = password.replace(/\\(.)/g, '$1');
             const result = await Auth(username, pass);
             setAuth(result);
-        } catch (error) {
-            console.error('Error during authentication:', error);
+            setError(null); // Reset error on successful login
+        } catch (authError) {
+            console.error('Error during authentication:', authError);
             setAuth(null);
+            setError('Incorrect username or password. Please try again.');
+    
         }
     };
-
+    
+    
 
     return (
         <div className="container mx-auto p-4">
             <div className="bg-white p-8 rounded shadow-md max-w-md mx-auto">
                 <h2 className="text-2xl font-bold mb-4">Login Form</h2>
+
                 <form onSubmit={handleLogin}>
                     <label htmlFor="username" className="block text-gray-700 text-sm font-semibold mb-2">
                         Username:
@@ -36,6 +40,7 @@ const LoginForm: React.FC = () => {
                         type="text"
                         id="username"
                         name="username"
+                        aria-label="Username"
                         className="w-full py-2 px-3 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
                         value={username}
                         onChange={(e) => setUsername(e.target.value)}
@@ -46,9 +51,10 @@ const LoginForm: React.FC = () => {
                         Password:
                     </label>
                     <input
-                        type="text"
+                        type="password"
                         id="password"
                         name="password"
+                        aria-label="Password"
                         className="w-full py-2 px-3 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
@@ -58,6 +64,12 @@ const LoginForm: React.FC = () => {
                     <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-700 mt-4">
                         Login
                     </button>
+                    {error &&(
+                    <div role="alert" className="text-red-600 mb-4">
+                        Invalid username or password. Please try again.
+                    </div>
+                )}
+            <div>{error}</div>
                 </form>
             </div>
         </div>
--- frontend/src/components/ProjectCard.tsx
+++ frontend/src/components/ProjectCard.tsx
@@ -1,133 +1,117 @@
 import React, { useState } from 'react';
-import * as schemas from "../models/api_schemas";
-import { pkgByID } from '../API/PkgbyID';
 import UpdateForm from './UpdateForm';
+import { pkgByID } from '../API/PkgbyID';
 import { useAuth } from './AuthContext';
-import { FaAutoprefixer } from 'react-icons/fa';
 import JSZip from 'jszip';
+
 interface ProjectCardProps {
-    Name: string;
-    Version: string;
-    ID: string;
-    Scores: {
-        BusFactor: number;
-        Correctness: number;
-        RampUp: number;
-        ResponsiveMaintainer: number;
-        GoodPinningPractice: number;
-        PullRequest: number;
-        NetScore: number;
-    }[];
+  Name: string;
+  Version: string;
+  ID: string;
+  Scores: {
+    BusFactor: number;
+    Correctness: number;
+    RampUp: number;
+    ResponsiveMaintainer: number;
+    GoodPinningPractice: number;
+    PullRequest: number;
+    NetScore: number;
+  }[];
 }
 
 const ProjectCard: React.FC<ProjectCardProps> = ({ Name, Version, ID, Scores }) => {
-    const [isUpdateFormOpen, setIsUpdateFormOpen] = useState<boolean>(false);
-    const handleUpdateClick = () => {
-        setIsUpdateFormOpen(true);
-    };
-    const handleUpdateFormClose = () => {
-        setIsUpdateFormOpen(false);
-    };
-    const { authResult } = useAuth();
-    let authResult1 = authResult;
-    if (authResult) {
-        authResult1 = authResult.replaceAll("\"", "");
+  const [isUpdateFormOpen, setIsUpdateFormOpen] = useState<boolean>(false);
+
+  const handleUpdateClick = () => {
+    setIsUpdateFormOpen(true);
+  };
+
+  const handleUpdateFormClose = () => {
+    setIsUpdateFormOpen(false);
+  };
+  const { authResult } = useAuth();
+  let authResult1 = authResult;
+  if (authResult) {
+      authResult1 = authResult.replaceAll("\"", "");
+  }
+  const handleDownloadClick = async () => {
+    try {
+        const result = await pkgByID(ID, authResult1);
+
+      if (result !== null) {
+        const bzip = atob(result.data.Content);
+        const zip = new JSZip();
+
+        zip.file('package.zip', atob(result.data.Content), { binary: true });
+
+        const blob = await zip.generateAsync({ type: 'blob' });
+
+        const downloadLink = document.createElement('a');
+        downloadLink.href = window.URL.createObjectURL(blob);
+        downloadLink.download = 'package.zip';
+
+        document.body.appendChild(downloadLink);
+        downloadLink.click();
+
+        document.body.removeChild(downloadLink);
+      } else {
+        console.error('Error in Download API call:', 'Failed to fetch package details.');
+      }
+    } catch (error) {
+      console.error('Error in Download API call:', error);
     }
+  };
 
-    const handleDownloadClick = async () => {
-        try {
-            // Call the API to get package details by ID
-            const result = await pkgByID(ID, authResult1);
-
-            if (result !== null) {
-                // Handle the API response as needed
-                console.log('Download API Response:', result);
-                const bzip = atob(result.data.Content)
-                const zip = new JSZip();
-
-                // Add the binary content to the zip file
-                zip.file('package.zip', atob(result.data.Content), { binary: true });
-
-                // Generate a Blob from the zip file
-                const blob = await zip.generateAsync({ type: 'blob' });
-
-                // Create a download link
-                const downloadLink = document.createElement('a');
-                downloadLink.href = window.URL.createObjectURL(blob);
-                downloadLink.download = 'package.zip'; // Specify the desired file name
-
-                // Trigger the download
-                document.body.appendChild(downloadLink);
-                downloadLink.click();
-
-                // Clean up
-                document.body.removeChild(downloadLink);
-                // Implement the logic to handle the download based on the API response
-            } else {
-                console.error('Error in Download API call:', 'Failed to fetch package details.');
-            }
-        } catch (error) {
-            console.error('Error in Download API call:', error);
-        }
-    };
-
-    return (
-        <div className="flex justify-end items-start px-5 py-4 relative">
-            {/* Update and Download buttons */}
-
-
-            <div className="w-full bg-white p-6 rounded-lg shadow-md transition-transform hover:shadow-lg transform hover:scale-95 hover:bg-gray-200">
-                {/* Project Name */}
-                <div className="absolute top-0 right-0 flex space-x-2 mt-2 mr-2">
-                    <button
-                        onClick={handleUpdateClick}
-                        className="bg-blue-500 text-white px-2 py-1 rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue"
-                    >
-                        Update
-                    </button>
-                    <button
-                        onClick={handleDownloadClick}
-                        className="bg-green-500 text-white px-2 py-1 rounded-md hover:bg-green-600 focus:outline-none focus:shadow-outline-green"
-                    >
-                        Download
-                    </button>
-
-                </div>
-                <h2 className="text-xl font-bold mb-2">Name: {Name}</h2>
-                <h2 className="text-xl font-bold mb-2">Version: {Version}</h2>
-
-                {/* Ratings */}
-                <div className="flex items-center space-x-2">
-                    <span className="text-lg font-bold">Ratings:</span>
-
-                    <div className="flex items-center">
-                        {/* Display the scores */}
-                        {Scores.map((score, index) => (
-                            <div key={index} className="flex flex-row items-center space-y-1">
-                                {/* Display individual score properties */}
-                                <div className="text-sm p-2">{`Bus Factor: ${score.BusFactor}`}</div>
-                                <div className="text-sm p-2">{`Correctness: ${score.Correctness}`}</div>
-                                <div className="text-sm p-2">{`Ramp Up: ${score.RampUp}`}</div>
-                                <div className="text-sm p-2">{`Responsive Maintainer: ${score.ResponsiveMaintainer}`}</div>
-                                <div className="text-sm p-2">{`Good Pinning Practice: ${score.GoodPinningPractice}`}</div>
-                                <div className="text-sm p-2">{`Pull Request: ${score.PullRequest}`}</div>
-                                <div className="text-sm p-2">{`Net Score: ${score.NetScore}`}</div>
-                                {/* Add more properties as needed */}
-                            </div>
-                        ))}
-                    </div>
-                </div>
-            </div>
-            {isUpdateFormOpen && (
-                <UpdateForm
-                    onClose={handleUpdateFormClose}
-                    Name={Name}
-                    ID={ID}
-                    Version={Version}
-                />
-            )}
+  return (
+    <div className="flex justify-end items-start px-5 py-4 relative">
+      <div className="w-full bg-white p-6 rounded-lg shadow-md transition-transform hover:shadow-lg transform hover:scale-95 hover:bg-gray-200">
+        <div className="absolute top-0 right-0 flex space-x-2 mt-2 mr-2">
+          <button
+            role="button"
+            aria-label="Update Package"
+            onClick={handleUpdateClick}
+            className="bg-blue-500 text-white px-2 py-1 rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue"
+          >
+            Update
+          </button>
+          <button
+            role="button"
+            aria-label="Download Package"
+            onClick={handleDownloadClick}
+            className="bg-green-500 text-white px-2 py-1 rounded-md hover:bg-green-600 focus:outline-none focus:shadow-outline-green"
+          >
+            Download
+          </button>
         </div>
-    );
+        <h2 className="text-xl font-bold mb-2">Name: {Name}</h2>
+        <h2 className="text-xl font-bold mb-2">Version: {Version}</h2>
+        <div className="flex items-center space-x-2">
+          <span className="text-lg font-bold">Ratings:</span>
+          <div className="flex items-center">
+            {Scores.map((score, index) => (
+              <div key={index} className="flex flex-row items-center space-y-1">
+                <div className="text-sm p-2">{`Bus Factor: ${score.BusFactor}`}</div>
+                <div className="text-sm p-2">{`Correctness: ${score.Correctness}`}</div>
+                <div className="text-sm p-2">{`Ramp Up: ${score.RampUp}`}</div>
+                <div className="text-sm p-2">{`Responsive Maintainer: ${score.ResponsiveMaintainer}`}</div>
+                <div className="text-sm p-2">{`Good Pinning Practice: ${score.GoodPinningPractice}`}</div>
+                <div className="text-sm p-2">{`Pull Request: ${score.PullRequest}`}</div>
+                <div className="text-sm p-2">{`Net Score: ${score.NetScore}`}</div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+      {isUpdateFormOpen && (
+        <UpdateForm
+          onClose={handleUpdateFormClose}
+          Name={Name}
+          ID={ID}
+          Version={Version}
+        />
+      )}
+    </div>
+  );
 };
 
 export default ProjectCard;
